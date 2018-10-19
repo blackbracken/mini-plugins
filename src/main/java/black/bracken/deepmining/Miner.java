523facc5fafc234b7ab650809c733b0cbbfd98da
BlackBracken <me@bracken.black>
2018-10-19 11:55:48 +0900

    removes ugly static fields

--- src/main/java/black/bracken/deepmining/Miner.java
+++ src/main/java/black/bracken/deepmining/Miner.java
@@ -2,36 +2,20 @@ package black.bracken.deepmining;
 
 import black.bracken.deepmining.event.RelationalBlockBreakEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
-
-import java.util.Set;
 
 /**
  * @author BlackBracken
  */
 public final class Miner {
 
-    private static final DeepMining instance;
-    private static final int maxDepth;
-    private static final long mineDelay;
-    private static final Set<BlockFace> relationSet;
-    private static final Set<Material> oreMaterialSet;
-
-    static {
-        instance = DeepMining.getInstance();
-        maxDepth = instance.getMaxDepth();
-        mineDelay = instance.getMineDelay();
-        relationSet = instance.getRelationSet();
-        oreMaterialSet = instance.getOreMaterialSet();
-    }
-
+    private final DeepMining instance;
     private final Block broken;
     private final Player breaker;
 
     public Miner(Block broken, Player breaker) {
+        this.instance = DeepMining.getInstance();
         this.broken = broken;
         this.breaker = breaker;
     }
@@ -41,7 +25,7 @@ public final class Miner {
     }
 
     private void mineRelations(Block broken, Player breaker, int depth) {
-        if (maxDepth <= depth || !oreMaterialSet.contains(broken.getType())) return;
+        if (instance.getMaxDepth() <= depth || !instance.getOreMaterialSet().contains(broken.getType())) return;
 
         RelationalBlockBreakEvent event = new RelationalBlockBreakEvent(broken, breaker, depth);
         instance.getServer().getPluginManager().callEvent(event);
@@ -50,8 +34,8 @@ public final class Miner {
 
         Bukkit.getScheduler().runTaskLater(
                 instance,
-                () -> relationSet.forEach(relation -> this.mineRelations(broken.getRelative(relation), breaker, depth + 1)),
-                mineDelay
+                () -> instance.getRelationSet().forEach(relation -> this.mineRelations(broken.getRelative(relation), breaker, depth + 1)),
+                instance.getMineDelay()
         );
     }
 
