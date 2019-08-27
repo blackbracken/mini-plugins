f6876fcaf7f0a8f573f34ed470918305542c3c4b
BlackBracken <me@bracken.black>
2019-08-27 21:40:11 +0900

    initialize project

---
+++ src/main/java/black/bracken/airfoil/Airfoil.java
@@ -0,0 +1,11 @@
+package black.bracken.airfoil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Airfoil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
