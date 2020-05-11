d33f2d3bcfe3df7d045d39ae4074f597933cd572
blackbracken <me@bracken.black>
2020-05-11 22:35:32 +0900

    initialize project

---
+++ src/main/java/black/bracken/oakin/Oakin.java
@@ -0,0 +1,11 @@
+package black.bracken.oakin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Oakin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
