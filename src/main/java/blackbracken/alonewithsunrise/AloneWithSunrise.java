8464b6c254aae5e8fbb322d7d93c4f5aa9455e6a
blackbracken <me@bracken.black>
2021-09-17 01:24:50 +0900

    Initial commit

---
+++ src/main/java/blackbracken/alonewithsunrise/AloneWithSunrise.java
@@ -0,0 +1,41 @@
+package blackbracken.alonewithsunrise;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+import java.util.Objects;
+
+public final class AloneWithSunrise extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEnterToBend(PlayerBedEnterEvent event) {
+        Player player = event.getPlayer();
+        Location particleLocation = event.getBed().getLocation()
+                .clone()
+                .add(0.0, 1.0, 0.0);
+        World world = Objects.requireNonNull(particleLocation.getWorld());
+
+        if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+            player.getWorld().setTime(0);
+            world.spawnParticle(Particle.VILLAGER_HAPPY, particleLocation, 12, 0.5, 0.5, 0.5);
+        }
+    }
+
+}
