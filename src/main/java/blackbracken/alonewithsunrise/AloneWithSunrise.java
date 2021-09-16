bb11679eb277e723a8ed6bdd679df1c75232a6ce
blackbracken <me@bracken.black>
2021-09-17 01:48:27 +0900

    set time as tomorrow

--- src/main/java/blackbracken/alonewithsunrise/AloneWithSunrise.java
+++ src/main/java/blackbracken/alonewithsunrise/AloneWithSunrise.java
@@ -4,10 +4,10 @@ import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
@@ -33,7 +33,7 @@ public final class AloneWithSunrise extends JavaPlugin implements Listener {
         World world = Objects.requireNonNull(particleLocation.getWorld());
 
         if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
-            player.getWorld().setTime(0);
+            player.getWorld().setTime(world.getTime() + 26000 - world.getTime() % 24000);
             world.spawnParticle(Particle.VILLAGER_HAPPY, particleLocation, 12, 0.5, 0.5, 0.5);
         }
     }
